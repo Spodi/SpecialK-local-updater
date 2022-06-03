49c89c148764aabf6560ce47954cc3be72c76c34
Spodi <spodi2290@aol.com>
2022-06-03 22:48:28 +0200

    Added wrapper batch for easy start

---
+++ SK_LU.bat
@@ -0,0 +1,15 @@
+:: This is a wrapper to start the PowerShell script in the
+:: same directory with the same name automatically.
+:: PowerShell is a bit paranoid about starting scripts otherwise.
+:: This also takes care of things if called from a 32-bit app.
+@echo off
+title SKSS
+color 17
+if exist "%systemroot%\SysNative\WindowsPowerShell\v1.0\powershell.exe" (
+	echo Starting %systemroot%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
+	start %systemroot%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
+) else (
+	echo Starting %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
+	start %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
+)
+EXIT /b
