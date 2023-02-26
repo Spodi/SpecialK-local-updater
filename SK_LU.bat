5eeef292b1df1eccc37ca1526b33a2ce2e6d572d
Spodi <spodi2290@aol.com>
2023-02-26 19:42:16 +0100

    call instead of starting pwsh (only one window)

--- SK_LU.bat
+++ SK_LU.bat
@@ -3,14 +3,16 @@
 :: PowerShell is a bit paranoid about starting scripts otherwise.
 :: This also takes care of things if called from a 32-bit app.
 @echo off
-title SKSS
-color 17
-cd %~dp0
+title SKLU
+
 if exist "%systemroot%\SysNative\WindowsPowerShell\v1.0\powershell.exe" (
 	echo Starting %systemroot%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
-	start %systemroot%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
+	echo.
+	call %systemroot%\SysNative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
 ) else (
 	echo Starting %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
-	start %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
+	echo.
+	call %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -File "%~dpn0.ps1" %*
 )
+
 EXIT /b
